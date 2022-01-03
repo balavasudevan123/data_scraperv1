f3a80b2b23cd1d23634124b4d96a13f6487fac20
balamuruganv@compunetconnections.com <balamuruganv@compunetconnections.com>
2022-01-03 13:14:30 +0000

    File generated by Skaffolder - user: balamuruganv@compunetconnections.com

---
+++ api/properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_data_scraperv1_db_Url = $_ENV["DB_HOST"] ?? '127.0.0.1';
+	$db_data_scraperv1_db_User = 'root';
+	$db_data_scraperv1_db_Pass = '';
+	$db_data_scraperv1_db_DbName = 'data_scraperv1_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
